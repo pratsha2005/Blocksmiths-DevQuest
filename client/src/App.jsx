4550e3df36073f16efb026e7956bfc92bd5791b2
Gourav <gouravpanchal1632@gmail.com>
2025-01-18 04:46:27 +0530

    Fix bugs in app.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,28 @@
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
-import Layout from './components/Layout.jsx';
+import { BrowserRouter as Router, Route, Routes, useNavigate, useLocation } from 'react-router-dom';
 import Login from './pages/Login.jsx';
 import SignUp from './pages/SignUp.jsx';
 import Dashboard from './pages/Dashboard.jsx';
 import SignInOptions from './pages/SignInOption.jsx';
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Layout />}>
-      {/* Uncomment this if you have a Home component */}
-      {/* <Route path="/" element={<Home />} /> */}
-      <Route path="signInAs" element={<SignInOptions />} />
-      <Route path="login" element={<Login />} />
-      <Route path="register" element={<SignUp />} />
-      <Route path="dashboard" element={<Dashboard />} />
-    </Route> // Fixed this closing tag
-  )
-);
-
-function App() {
-  return (
-    <>
-      <RouterProvider router={router} />
-    </>
+import Home from './pages/Home.jsx'
+function AppContent(){
+  return(
+    <Routes>
+      
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<SignUp />} />
+      <Route path="/signInAs" element={<SignInOptions />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
   );
 }
 
+
+function App() {
+  return (
+      <Router>
+        <AppContent />
+      </Router> 
+  );
+}
 export default App;
