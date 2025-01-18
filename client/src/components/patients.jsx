92c16e9100bb801b18a4612b72a029bc144d9a9c
Gourav <gouravpanchal1632@gmail.com>
2025-01-18 11:47:41 +0530

    Card component

--- client/src/components/patients.jsx
+++ client/src/components/patients.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import SearchBar from './searchBar'
+import PatientCard from './patientCard'
+import { PatientRecords } from './patientRecords'
 
 function Patients() {
   return (
     <div>
       <SearchBar />
+      <PatientCard />
+
     </div>
   )
 }
