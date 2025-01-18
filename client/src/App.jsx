e4942ec16d06843249ff2c851635ada988c7fc81
Gourav <gouravpanchal1632@gmail.com>
2025-01-18 16:40:47 +0530

    Add navigation to login and sign up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Route, Routes, useNavigate, useLocation } from 'react-router-dom';
-import Login from './pages/Login.jsx';
-import SignUp from './pages/SignUp.jsx';
+import DoctorLogin from './pages/DoctorLogin.jsx';
+import DoctorSignUp from './pages/DoctorSignUp.jsx';
+import PatientLogin from './pages/PatientLogin.jsx';
+import PatientSignUp from './pages/PatientSignUp.jsx';
 import PatientDashboard from './pages/PatientDashboard.jsx';
 import SignInOptions from './pages/SignInOption.jsx';
 import Home from './pages/Home.jsx'
@@ -10,8 +12,10 @@ function AppContent(){
   return(
     <Routes>
       
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<SignUp />} />
+      <Route path="/doctor/login" element={<DoctorLogin />} />
+      <Route path="/doctor/register" element={<DoctorSignUp />} />
+      <Route path="/patient/login" element={<PatientLogin />} />
+      <Route path="/patient/register" element={<PatientSignUp />} />
       <Route path="/signInAs" element={<SignInOptions />} />
       <Route path="/" element={<Home />} />
       <Route path="/doctor/dashboard" element={<DoctorDashboard />} />
