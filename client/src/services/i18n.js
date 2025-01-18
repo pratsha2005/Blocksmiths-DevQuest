6cfbd2b953ff08286fbde2227e90bd6ae0dcf17c
pratsha2005 <pratsha2005@gmail.com>
2025-01-18 17:55:25 +0530

    TranslationSuccess

--- client/src/services/i18n.js
+++ client/src/services/i18n.js
@@ -27,7 +27,7 @@ i18n.use(initReactI18next).init({
                     desc1 : 'Integrates seamlessly with various healthcare systems and data sources, providing a unified and comprehensive health history.',
                     subtitle2 : 'Trust and Transparency',
                     desc2: 'All changes to your health records are recorded transparently on the blockchain, ensuring data integrity and trust ',
-                    subtitle3 : 'Security',
+                    subtitle3 : 'Smart Contracts and Automation',
                     desc3: 'Leveraging blockchain technology to ensure your health records are safe, immutable, and accessible only by authorized individuals.',
                     subtitle4: 'Ease of Use',
                     desc4: 'Simplify insurance claims and billing processes by securely sharing verified health data with authorized entities.',
@@ -45,7 +45,6 @@ i18n.use(initReactI18next).init({
                     About: 'के बारे में',
                     Services: 'सेवाएं',
                     Contact: 'संपर्क',
-                }
                 },
                 landing: {
                     title: 'स्वास्थ्य सुरक्षा',
@@ -59,16 +58,16 @@ i18n.use(initReactI18next).init({
                     desc1 : 'एक एकीकृत और व्यापक स्वास्थ्य इतिहास प्रदान करते हुए, विभिन्न स्वास्थ्य देखभाल प्रणालियों और डेटा स्रोतों के साथ सहजता से एकीकृत होता है।',
                     subtitle2 : 'विश्वास और पारदर्शिता',
                     desc2: 'आपके स्वास्थ्य रिकॉर्ड में सभी परिवर्तन ब्लॉकचेन पर पारदर्शी रूप से दर्ज किए जाते हैं, जिससे डेटा अखंडता और विश्वास सुनिश्चित होता है',
-                    subtitle3 : 'सुरक्षा',
-                    desc3: 'यह सुनिश्चित करने के लिए ब्लॉकचेन तकनीक का लाभ उठाना कि आपके स्वास्थ्य रिकॉर्ड सुरक्षित, अपरिवर्तनीय और केवल अधिकृत व्यक्तियों द्वारा ही पहुंच योग्य हैं।',
+                    subtitle3 : 'स्वचालन के लिए स्मार्ट अनुबंध',
+                    desc3: 'स्मार्ट कॉन्ट्रैक्ट बीमा दावों, प्रिस्क्रिप्शन रिफिल और डेटा शेयरिंग समझौतों जैसी प्रक्रियाओं को स्वचालित कर सकते हैं, जिससे ये प्रक्रियाएं तेज और बाधा मुक्त हो जाती हैं।',
                     subtitle4: 'उपयोग में आसानी',
                     desc4: 'अधिकृत संस्थाओं के साथ सत्यापित स्वास्थ्य डेटा को सुरक्षित रूप से साझा करके बीमा दावों और बिलिंग प्रक्रियाओं को सरल बनाएं।',
-                    subtitle5: 'Security',
-                    desc5 : 'Leveraging blockchain technology to ensure your health records are safe, immutable, and accessible only by authorized individuals.',
+                    subtitle5: 'सुरक्षा',
+                    desc5 : 'यह सुनिश्चित करने के लिए ब्लॉकचेन तकनीक का लाभ उठाना कि आपके स्वास्थ्य रिकॉर्ड सुरक्षित, अपरिवर्तनीय और केवल अधिकृत व्यक्तियों द्वारा ही पहुंच योग्य हैं।',
                     subtitle6: 'विश्वव्यापी पहुँच',
                     desc6: 'कहीं से भी पहुंच योग्य, अंतरराष्ट्रीय स्तर पर यात्रा करने वाले या स्थानांतरित होने वाले मरीजों की देखभाल की सुविधा' ,
                 }
             }
         }
     }
-)
+})
