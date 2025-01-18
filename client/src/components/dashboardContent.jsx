66f415b3af32ef769e17e6c7dd76764544825e69
Gourav <gouravpanchal1632@gmail.com>
2025-01-18 11:31:59 +0530

    Change pageheading dynamically

--- client/src/components/dashboardContent.jsx
+++ client/src/components/dashboardContent.jsx
@@ -3,9 +3,15 @@ import PageHeading from "./pageHeading";
 import Patients from "./patients";
 import RequestAccess from './RequestAccess'
 const DashboardContent = ({ selectedOption }) => {
+  let heading;
+  if (selectedOption === 'patients') {
+    heading = "Patients";
+  } else if (selectedOption === 'requestAccess') {
+    heading = "Request Access";
+  }
   return (
     <div className="p-6">
-      <PageHeading />
+      <PageHeading heading={heading} />
       <div className="mt-4">
         {selectedOption === 'patients' && <Patients />}
         {selectedOption === 'requestAccess' && <RequestAccess />}
--- client/src/components/patients.jsx
+++ client/src/components/patients.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import SearchBar from './searchBar'
 
 function Patients() {
   return (
     <div>
-      hello
+      <SearchBar />
     </div>
   )
 }
